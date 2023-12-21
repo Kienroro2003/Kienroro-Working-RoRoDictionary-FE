03a29d960a19e199c65ed329bd950c17251b698d
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-12-21 23:54:44 +0700

    config color and font size continue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="p-10">
+        <h1 className="h-10 bg-primary-light-90 text-heading-1">Heading 1</h1>
+      </div>
+    </Fragment>
   );
 }
 
