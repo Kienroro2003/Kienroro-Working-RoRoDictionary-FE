d814c1b79cf9e04fd439e4ab006bd0693666bde0
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-12-25 22:50:46 +0700

    update input-text input-radio input-checkbox input-select

--- src/pages/TestUIStyles.js
+++ src/pages/TestUIStyles.js
@@ -1,4 +1,5 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import useClickOutSide from "../hooks/useClickOutSide";
 
 const TestUIStyles = () => {
   return (
@@ -8,8 +9,75 @@ const TestUIStyles = () => {
           Heading 1
         </h1>
         <input type="text" className="input-text" placeholder="Hello" />
+        <Checkbox></Checkbox>
+        <Radio></Radio>
+        <InputSelect></InputSelect>
       </div>
     </Fragment>
+  );
+};
+
+const Radio = () => {
+  return (
+    <div>
+      <input type="radio" name="" id="radio" />
+      <label htmlFor="radio" className="input-radio"></label>
+    </div>
+  );
+};
+
+const Checkbox = () => {
+  return (
+    <div className="">
+      <input type="checkbox" name="" id="checkbox" />
+      <label
+        className="block w-8 h-8 border border-black-30 rounded-lg  relative checkbox-inner cursor-pointer transition-all"
+        htmlFor="checkbox"
+      >
+        <div className="absolute w-5 h-3 top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/3 border-b-2 border-l-2 border-b-white border-l-white rounded-sm -rotate-45 opacity-0 invisible transition-all"></div>
+      </label>
+    </div>
+  );
+};
+
+const InputSelect = () => {
+  const { show, setShow, nodeRef } = useClickOutSide();
+  const [label, setLabel] = useState("Dropdown");
+
+  const handleValueClick = (e) => {
+    setLabel("Your select is: " + e.target.dataset.value);
+    setShow(false);
+  };
+  return (
+    <div className="relative" ref={nodeRef}>
+      <div
+        className="py-6 px-8 border border-white-30 rounded-2xl text-body-5 text-black-30 flex items-center justify-between cur"
+        onClick={() => setShow(!show)}
+      >
+        <span>{label}</span>
+        <img src="assets/icons/arrow-down.svg" alt="" />
+      </div>
+      {show && (
+        <div
+          className="absolute rounded-2xl border border-white-30
+ top-full w-full left-0  flex flex-col translate-y-4 overflow-hidden"
+        >
+          {Array(5)
+            .fill(0)
+            .map((item, index) => {
+              return (
+                <div
+                  className=" py-6 px-8 text-body-5 text-black-30 hover:bg-primary-light-90"
+                  onClick={handleValueClick}
+                  data-value={"Dropdown 1"}
+                >
+                  <span>Dropdown 1</span>
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </div>
   );
 };
 
