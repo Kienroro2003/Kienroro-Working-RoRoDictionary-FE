4164a19e95f2f721c25cb64a2165d872306ab2ea
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-12-28 22:33:50 +0700

    update navbar and navbar item

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
 import TestUIStules from "./pages/TestUIStyles";
 import TestUIStyles from "./pages/TestUIStyles";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <Routes>
-      <Route
-        path="/test-ui-styles"
-        element={<TestUIStyles></TestUIStyles>}
-      ></Route>
+      <Route element={<Sidebar></Sidebar>}>
+        <Route path="/" element={<></>}></Route>
+        <Route
+          path="/test-ui-styles"
+          element={<TestUIStyles></TestUIStyles>}
+        ></Route>
+      </Route>
     </Routes>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <Fragment>
+      <div className="px-10 py-6 flex flex-col w-[300px] gap-16">
+        <NavLink to="/">
+          <img src="/assets/images/Logo.svg" alt="Logo" />
+        </NavLink>
+
+        <nav className="navbar">
+          <NavbarItem link="/">Overview</NavbarItem>
+          <NavbarItem link="/test-ui-styles">Test</NavbarItem>
+        </nav>
+      </div>
+      <Outlet></Outlet>
+    </Fragment>
+  );
+};
+
+const NavbarItem = ({ link, children, className }) => {
+  return (
+    <NavLink
+      to={link}
+      className={({ isActive }) =>
+        `navbar__link ${className} ${isActive ? "active" : ""}`
+      }
+    >
+      {children}
+    </NavLink>
+  );
+};
+
+export default Sidebar;
