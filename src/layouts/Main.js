10ebbd62eeb6f7fa1271c0ce709c28f806504886
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-12-28 23:01:39 +0700

    update style outlet

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -6,7 +6,32 @@ const Main = () => {
   return (
     <div className="flex">
       <Sidebar></Sidebar>
-      <div className="flex-auto">
+      <div className="flex-auto bg-white-10 min-h-screen">
+        <div className="flex justify-center py-6">
+          <form action="" className="relative pl-10">
+            <input
+              type="text"
+              className="text-body-6 w-[260px] placeholder:text-black-60 py-3 px-4"
+              placeholder="Search Appointment, Patient or etc"
+            />
+            <button className="absolute top-0 left-0 bottom-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-8 h-8"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </button>
+          </form>
+        </div>
         <Outlet></Outlet>
       </div>
     </div>
