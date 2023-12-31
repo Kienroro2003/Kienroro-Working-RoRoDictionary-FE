73526460593363c02f9e0211e32c1e4bfa518627
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-12-31 23:41:53 +0700

    update file helper

--- src/pages/Analysis.js
+++ src/pages/Analysis.js
@@ -1,11 +1,21 @@
 import React, { Fragment } from "react";
 import Button from "../components/Button";
+import { useForm } from "react-hook-form";
 
 const Analysis = () => {
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    // formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data.analysis.replaceAll("\n", " "));
+  };
   return (
     <Fragment>
       <h2 className="text-black">Paragraph Analysis</h2>
-      <form action="" className="mt-8">
+      <form action="" className="mt-8" onSubmit={handleSubmit(onSubmit)}>
         <label
           htmlFor="analysis"
           className=" block text-body-5 text-black-10 mb-4"
@@ -13,15 +23,15 @@ const Analysis = () => {
           Description
         </label>
         <textarea
-          name="analysis"
+          {...register("analysis")}
           id="analysis"
           className="block resize-none w-full h-[300px] rounded-2xl border border-black-80 bg-transparent px-4 py-2 mb-10"
         ></textarea>
         <div className="flex">
           <Button
-            type="button"
+            type="submit"
             secondary
-            onClick={() => console.log("Hello")}
+            // onClick={(e) => e.preventDefault()}
             className="ml-auto"
           >
             Analysis
